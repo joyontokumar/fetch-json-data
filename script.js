d14f57a88fead834255bb3a1e31076dcb2ea83a7
joyonto kumar <engr.joyontoroy@gmail.com>
2019-06-07 17:01:28 +0600

    single json and multiple json create

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.getElementById('btn1').addEventListener('click', loadjsondata);
+// single json data
+document.getElementById('btn1').addEventListener('click', loadjsondata1);
 
-function loadjsondata() {
+function loadjsondata1() {
     fetch('myJson.json')
         .then(responseData => {
             return responseData.json();
@@ -13,7 +14,34 @@ function loadjsondata() {
                 <li class="list-group-item"> city : ${data.phoneNumber}</li>
                 <li class="list-group-item"> city : ${data.gender}</li>
             </ul>`;
-            document.getElementById('output').innerHTML = output;
+            document.getElementById('singlejson').innerHTML = output;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+// multiple json data
+
+document.getElementById('btn2').addEventListener('click', loadjsondata2);
+
+function loadjsondata2() {
+    fetch('myJson2.json')
+        .then(responseData => {
+            return responseData.json();
+        })
+        .then(data => {
+            let output = '';
+            data.forEach(function (multileItem) {
+                output += `<ul class="list-group mt-5"> 
+                <li class="list-group-item"> id : ${multileItem.id}</li>
+                <li class="list-group-item"> name : ${multileItem.name}</li>
+                <li class="list-group-item"> city : ${multileItem.city}</li>
+                <li class="list-group-item"> city : ${multileItem.phoneNumber}</li>
+                <li class="list-group-item"> city : ${multileItem.gender}</li>
+            </ul>`;
+            })
+            document.getElementById('multiplejson').innerHTML = output;
         })
         .catch(err => {
             console.log(err);
